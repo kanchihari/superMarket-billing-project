7b233c8eb4c61ca66d690c7b0f00d799a4567649
kanchihari <>
2023-09-18 22:46:32 +0000

    Add main program with user welcome, name prompt,and item list

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from datetime import datetime
+
+# Main program 
+print("--------------------- Welcome to SuperMarket ---------------------")
+
+# Prompt the user for their name
+name = input("Enter Your name: \n")
+
+# List of items along with their prices in euros per unit or per kilogram/liter
+lists = '''
+1. Apples - 1.50 €/- per kg
+2. Rice - 4.00 €/- per kg
+3. Milk - 2.00 €/- per liter
+4. Oil - 2.39 €/- per liter
+5. Meat - 14.00 €/- per kg
+6. Cola - 1.90 €/- per liter
+7. Wine - 2.50 €/- 500ml
+8. Curd - 1.40 €/- 500ml
+9. Atta - 5.00 €/- per kg
+10. Bread - 3.00 €/- per loaf
+'''
